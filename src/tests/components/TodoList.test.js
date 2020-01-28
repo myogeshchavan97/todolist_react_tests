c1dde1c7952b0e6dd5e9edbc55fa61c15c719429
Yogesh Chavan <ychavan@mdsol.com>
2020-01-28 11:13:09 +0530

    code coverage changes

--- src/tests/components/TodoList.test.js
+++ src/tests/components/TodoList.test.js
@@ -2,7 +2,25 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import TodoList from '../../components/TodoList';
 
+let wrapper;
+
+beforeAll(() => {
+  wrapper = shallow(<TodoList />);
+});
+
 test('should test TodoList component', () => {
-  const wrapper = shallow(<TodoList />);
   expect(wrapper).toMatchSnapshot();
+});
+
+test('should call handleAddTodo method', () => {
+  const instance = wrapper.instance();
+  const value = 'Publish Article';
+  instance.handleAddTodo({
+    preventDefault: () => {},
+    target: {
+      todo: {
+        value
+      }
+    }
+  });
 });
