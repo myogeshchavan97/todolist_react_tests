6e912d0675ab1f94044c6d2566732a1868aeae9f
Thomas Leveque <tleveque97@gmail.com>
2021-01-06 10:19:05 +0100

    removing a test to see what happens

--- src/tests/components/AddTodo.test.js
+++ src/tests/components/AddTodo.test.js
@@ -2,10 +2,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import AddTodo from '../../components/AddTodo';
 
-test('should test AddTodo component', () => {
-  const wrapper = shallow(<AddTodo />);
-  expect(wrapper).toMatchSnapshot();
-});
 
 test('should handle onSubmit handler', () => {
   const onSubmitSpy = jest.fn();
